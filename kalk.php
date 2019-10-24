1ad75a76560cff58270b467d63a77e6808112673
ZseeRad <56677527+ZseeRad@users.noreply.github.com>
2019-10-24 13:07:43 +0200

    Add files via upload

--- kalk.php
+++ kalk.php
@@ -5,22 +5,21 @@
 	<input type="radio" name="wybor" value="2"> odejmij<br>
 	<input type="radio" name="wybor" value="3"> pomnoz<br>
 	<input type="radio" name="wybor" value="4"> podziel<br>
+	<input type="radio" name="wybor" value="5"> potęguj<br>
+	<input type="radio" name="wybor" value="6"> pierwiastkuj<br>
 	<input type="submit">
 </form>
 
 <?php
-
 if(isset($_POST['liczba1']) && isset($_POST['liczba2']) && isset($_POST['wybor']))
 {
-
 	@$liczba1 = $_POST['liczba1'];
 	@$liczba2 = $_POST['liczba2'];
 	$wybor = $_POST['wybor'];
 	$wynik;
 	
-	//echo "$liczba1<br>";
-	//echo "$liczba2<br>";
-	//echo "$wybor<br>";
+	
+
 	switch($wybor)
 	{
 		case 1:
@@ -30,10 +29,23 @@ if(isset($_POST['liczba1']) && isset($_POST['liczba2']) && isset($_POST['wybor']
 			$wynik=$liczba1-$liczba2;
 			break;
 		case 3:
-			$wynik-$liczba1*$liczba2;
+			$wynik=$liczba1*$liczba2;
 			break;
 		case 4:
+			if($liczba2==0)
+			{
+				echo "Dzielnik jest zerem<br>";
+				$wynik=0;
+				break;
+			}
 			$wynik=$liczba1/$liczba2;
+			break;
+		case 5:
+			$wynik=pow($liczba1,$liczba2);
+			break;
+
+		case 6:
+			$wynik=pow($liczba1,1/$liczba2);
 			break;
 	}
 	echo @$wynik . "<br>";
